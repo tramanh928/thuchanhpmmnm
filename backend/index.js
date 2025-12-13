ecd6b9f7c89805ecce5e90f5a200dc46d34c1d66
tramanh928 <tram3994@gmail.com>
2025-12-13 22:42:41 +0700

    Cap nhat lai backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello Backend!'));
+app.get('/', (req, res) => res.send('Bùi Mai Trâm Anh_DH52200319'));
 app.listen(3000, () => console.log('Backend running on port 3000'));
