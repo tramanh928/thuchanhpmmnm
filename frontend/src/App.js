c5f7c09346aad5b78844f00011b195203bae3cd1
tramanh928 <tram3994@gmail.com>
2025-12-17 07:27:35 +0700

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Bùi Mai Trâm Anh</h1>
+      <h1>Bùi Mai Trâm Anh - DH52200319</h1>
+      <p>Ca học: Sáng thứ 4 - Ca 1</p>
+      <p>Test: 17/12/2025</p>
     </div>
   );
 }
